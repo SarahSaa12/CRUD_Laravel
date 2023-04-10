ae9bbf3d039767a6467e30df02e98c6ecc2b4ad1
SarahSaa12 <128454805+SarahSaa12@users.noreply.github.com>
2023-04-10 11:12:59 +0700

    commit

---
+++ storage/framework/views/df7b2f28d5038edc79c0d0bd79434e6f3e44f6bf.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Your Website 2023</span>
+        </div>
+    </div>
+</footer><?php /**PATH D:\Pendidikan Ilmu Komputer\Semester 6\htdocs\clone\Penjualan3\resources\views/layouts/footer.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/f2ed809014861bc53939b95b0804407da50bc878.php
@@ -0,0 +1,8 @@
+
+
+<?php $__env->startSection('title', 'Dashboard'); ?>
+
+<?php $__env->startSection('contents'); ?>
+
+    
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\Pendidikan Ilmu Komputer\Semester 6\htdocs\clone\Penjualan3\resources\views/dashboard.blade.php ENDPATH**/ ?>
